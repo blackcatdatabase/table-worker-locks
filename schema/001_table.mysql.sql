0fb8246e044e8a8d100f10d0340f6e4b83fbb033
Tolben <vito416@protonmail.com>
2025-12-18 00:27:24 +0100

    Fix: idx conflicts, fk (#6)

--- schema/001_table.mysql.sql
+++ schema/001_table.mysql.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map-mysql.yaml (map@sha1:B9D3BE28A74392B9B389FDAFB493BD80FA1F6FA4)
+-- Auto-generated from schema-map-mysql.yaml (map@sha1:7AAC4013A2623AC60C658C9BF8458EFE0C7AB741)
 -- engine: mysql
 -- table:  worker_locks
 
--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map-postgres.yaml (map@sha1:FAEA49A5D5F8FAAD9F850D0F430ED451C5C1D707)
+-- Auto-generated from schema-map-postgres.yaml (map@sha1:621FDD3D99B768B6A8AD92061FB029414184F4B3)
 -- engine: postgres
 -- table:  worker_locks
 
