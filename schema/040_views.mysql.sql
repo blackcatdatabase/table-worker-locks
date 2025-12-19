b3172e2b138b43821f054888e1e4a6eb30f7f890
Vito416 <vito416@protonmail.com>
2025-12-19 01:07:41 +0100

    Fix: Resolved outdated raw IP columns, regenerated from schema

--- schema/040_views.mysql.sql
+++ schema/040_views.mysql.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-views-mysql.yaml (map@sha1:B3C579FF17AC186C47D2C4AC86B0738DB2308BF2)
+-- Auto-generated from schema-views-mysql.yaml (map@sha1:39CF23914A48753BF55EEB1F38DDBA21AB1DBBB7)
 -- engine: mysql
 -- table:  worker_locks
 
